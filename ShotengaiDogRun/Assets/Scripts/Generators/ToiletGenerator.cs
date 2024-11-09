dabe4be3e2e2279d00ab4d74f8aefa04288abd23
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-10 03:13:03 +0900

    ToiletGenerator.csを追加

---
+++ ShotengaiDogRun/Assets/Scripts/Generators/ToiletGenerator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject[] toiletPrefabs;
+    private static readonly Vector3 initialPosition = new Vector3(0, -2, 0);
+    private static readonly int TOILET_COUNT = 10;  // 生成するゴミの数
+
+    void Start()
+    {
+        GenerateToilet();
+    }
+
+    void GenerateToilet()
+    {
+        if (toiletPrefabs.Length == 0)
+        {
+            Debug.LogWarning("ToiletGeneratorにPrefabが割り当てられていません！");
+            return;
+        }
+
+        for (int currentCount = 1; currentCount <= TOILET_COUNT; currentCount++)
+        {
+            float randomPosX = Random.Range(0, StageConstants.GROUND_X_COUNT);
+            int randomIndex = Random.Range(0, toiletPrefabs.Length);
+            GameObject selectedPrefab = toiletPrefabs[randomIndex];
+
+            if (selectedPrefab != null)
+            {
+                Vector3 randomPosition = new Vector3(randomPosX, initialPosition.y, initialPosition.z);
+                Instantiate(selectedPrefab, randomPosition, Quaternion.identity);
+            }
+        }
+    }
+}
