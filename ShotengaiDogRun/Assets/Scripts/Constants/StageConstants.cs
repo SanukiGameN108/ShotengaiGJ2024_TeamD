3ebb188f686d6505c9bc523eb9786c275433c535
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-10 03:56:55 +0900

    hollPosXListにX座標を指定すると、その箇所だけステージに穴が開くように変更

--- ShotengaiDogRun/Assets/Scripts/Constants/StageConstants.cs
+++ ShotengaiDogRun/Assets/Scripts/Constants/StageConstants.cs
@@ -9,6 +9,9 @@ public static class StageConstants
 {
     public static readonly Vector3 GROUND_CUBE_SCALE = new Vector3(1, 1, 1);
     public static readonly Vector3 INITIAL_POSITION = new Vector3(-20, -5, 0);
+
+    public static readonly int[] hollPosXList = {4, 5, 20, 21};
+
     public static readonly int GROUND_X_COUNT = 100;
     public static readonly int GROUND_Y_COUNT = 3;
     public static readonly int GROUND_Z_COUNT = 5;
