48350d2a11710d0439318095fa082a665e00bd9b
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-09 23:27:19 +0900

    GroundCubePrefabをx, zだけでなくy方向にも座標と個数を指定して配置できる機能を追加

--- ShotengaiDogRun/Assets/Scripts/GroundGenerator.cs
+++ ShotengaiDogRun/Assets/Scripts/GroundGenerator.cs
@@ -4,8 +4,9 @@ public class GroundGenerator : MonoBehaviour
 {
     public GameObject groundCubePrefab;
     private static readonly Vector3 GROUND_CUBE_SCALE = new Vector3(1, 1, 1);
-    private static readonly Vector3 INITIAL_POSITION = new Vector3(-20, -3, 0);
+    private static readonly Vector3 INITIAL_POSITION = new Vector3(-20, -5, 0);
     private static readonly int GROUND_X_COUNT = 100;
+    private static readonly int GROUND_Y_COUNT = 3;
     private static readonly int GROUND_Z_COUNT = 5;
 
     void Start()
@@ -23,13 +24,17 @@ public class GroundGenerator : MonoBehaviour
 
         for (int x = 0; x < GROUND_X_COUNT; x++)
         {
-            for (int z = 0; z < GROUND_Z_COUNT; z++)
+            for (int y = 0; y < GROUND_Y_COUNT; y++)
+            {
+                for (int z = 0; z < GROUND_Z_COUNT; z++)
             {
                 GameObject ground = Instantiate(groundCubePrefab);
                 ground.transform.localScale = GROUND_CUBE_SCALE;
                 // 配置位置を計算して設定
-                Vector3 position = INITIAL_POSITION + new Vector3(x * GROUND_CUBE_SCALE.x, 0, z * GROUND_CUBE_SCALE.z);
+                Vector3 diffPosition = new Vector3(x * GROUND_CUBE_SCALE.x, y * GROUND_CUBE_SCALE.y, z * GROUND_CUBE_SCALE.z);
+                Vector3 position = INITIAL_POSITION + diffPosition;
                 ground.transform.position = position;
+            }
             }
         }
     }
