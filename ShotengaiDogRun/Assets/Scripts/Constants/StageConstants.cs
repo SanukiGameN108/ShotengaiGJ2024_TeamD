1a96d1548101c4dde2393a68925b7ca4ee6324c1
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-10 16:57:40 +0900

    ステージ形成の定数を変更してステージを修正

--- ShotengaiDogRun/Assets/Scripts/Constants/StageConstants.cs
+++ ShotengaiDogRun/Assets/Scripts/Constants/StageConstants.cs
@@ -10,9 +10,13 @@ public static class StageConstants
     public static readonly Vector3 GROUND_CUBE_SCALE = new Vector3(1, 1, 1);
     public static readonly Vector3 INITIAL_POSITION = new Vector3(-20, -5, 0);
 
-    public static readonly int[] hollPosXList = {4, 5, 20, 21};
+    public static readonly int[] hollPosXList = {
+        4, 5, 20, 21, 70, 71, 72, 108, 109, 110, 
+        120, 121, 122, 145, 146, 147, 148, 150, 151, 152, 170, 171, 172, 
+        200, 201, 202, 210, 211, 212, 250, 251, 252, 270, 271, 272,
+    };
 
-    public static readonly int GROUND_X_COUNT = 100;
+    public static readonly int GROUND_X_COUNT = 350;
     public static readonly int GROUND_Y_COUNT = 3;
     public static readonly int GROUND_Z_COUNT = 5;
 }
