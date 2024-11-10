930952d2b075c49603e545d902625c4365f460ba
ProgramTantou <23051907@d.anabuki-college.net>
2024-11-10 11:15:13 +0900

    TimeManager

---
+++ ShotengaiDogRun/Assets/Nagao/Script/SystemScript/TimeManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TimeCounting();
+    }
+
+    //êßå¿éûä‘Çåvë™ÅB
+    void TimeCounting()
+    {
+        TimeSystem.instance.CountDownTime();
+        TimeSystem.instance.CountOneSecond();
+    }
+}
