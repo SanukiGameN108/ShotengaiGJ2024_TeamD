217a983b4042a68381436767d29d607f4311201d
ProgramTantou <23051907@d.anabuki-college.net>
2024-11-10 16:31:20 +0900

    HitColliderSyuusei

--- ShotengaiDogRun/Assets/Nagao/Script/PlayerScript/WideMove.cs
+++ ShotengaiDogRun/Assets/Nagao/Script/PlayerScript/WideMove.cs
@@ -8,6 +8,10 @@ public class WideMove : MoveSystem_Base
     [Tooltip("移動スピード")]
     private float MoveSpeed = 0;
 
+    [SerializeField]
+    [Tooltip("障害物に激突した時の吹っ飛ばされる距離")]
+    private float DamagePower = 1;
+
     public WideMove(): base()
     {
 
@@ -19,5 +23,11 @@ public class WideMove : MoveSystem_Base
         gameObject.transform.Translate(0,0, MoveSpeed);
     }
 
-     
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "DamageObject")
+        {
+            rb.AddForce(new Vector3(0,0,-MoveSpeed*DamagePower),ForceMode.Impulse);
+        }
+    }
 }
